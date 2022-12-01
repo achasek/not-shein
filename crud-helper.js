07b1e63bb4656fcae645fe7808cd006ab8820055
achasek <chaseknieper@gmail.com>
2022-12-01 15:45:52 -0700

    seed file finalized

--- crud-helper.js
+++ crud-helper.js
@@ -11,3 +11,33 @@ const User = require('./models/user');
 // Local variables will come in handy for holding retrieved documents
 let user, item, category, order;
 let users, items, categories, orders;
+
+// ------- DRAFT FOR STRIPE IMPLEMENTATION ------ //
+// const stripe = require('stripe')(process.env.SECRET_KEY);
+
+// app.post('/checkout', async (req, res) => {
+
+
+//     const items = req.body.items;
+//     let lineItems = [];
+//     items.forEach((item) => {
+//         lineItems.push(
+//             {
+//                 price: item.id,
+//                 quantity: item.quantity // change to qty later maybe 
+//             }
+//         )
+//     });
+
+//     const session = await stripe.checkout.sessions.create({
+//         line_items: lineItems,
+//         mode: 'payment', 
+//         success_url: 'http://localhost:3000',
+//         cancel_url: 'http://localhost:3000'
+//     });
+
+
+//     res.send(JSON.stringify({
+//         url: session.url
+//     }))
+// })
--- seed.js
+++ seed.js
@@ -15,10 +15,19 @@ require('./config/database');
 
     await Item.deleteMany({});
     const items = await Item.create([
-        {name: 'Cotton T-Shirt', category: categories[0], waterPrice: 2700, carbonPrice: 22800, image: '/images/shirt1.jpeg'},
-        {name: 'Slim Fit Denim', category: categories[1], waterPrice: 3800, carbonPrice: 33400, image: '/images/jeans1.jpeg'},
-        {name: 'Regular Fit Denim', category: categories[0], waterPrice: 3900, carbonPrice: 33400, image: '/images/jeans1.jpeg'},
-        {name: 'Blouse', category: categories[1], waterPrice: 3000, carbonPrice: 23000, image: '/images/jeans1.jpeg'},
+        {name: 'Cotton T-Shirt', category: categories[0], waterPrice: 2700, carbonPrice: 22800, image: '/images/shirt2.jpeg'},
+        {name: 'Slim Fit Denim', category: categories[1], waterPrice: 3800, carbonPrice: 33400, image: '/images/jeans2.jpeg'},
+        {name: 'Regular Fit Denim', category: categories[0], waterPrice: 3900, carbonPrice: 33400, image: '/images/jeans3.jpeg'},
+        {name: 'Blouse', category: categories[1], waterPrice: 3000, carbonPrice: 23000, image: '/images/blouse1.jpeg'},
+        {name: 'Cream Loose Fit Hoodie', category: categories[0], waterPrice: 3400, carbonPrice: 28000, image: '/images/hoodie1.jpeg'},
+        {name: 'Earth Crew Neck Pullover', category: categories[0], waterPrice: 3500, carbonPrice: 29000, image: '/images/sweater1.jpeg'},
+        {name: 'Loose Fit Denim', category: categories[1], waterPrice: 3900, carbonPrice: 33400, image: '/images/jeans4.jpeg'},
+        {name: 'Oversized Puffer Jacket', category: categories[0], waterPrice: 5900, carbonPrice: 52000, image: '/images/jacket1.jpeg'},
+        {name: 'Sequin Dress', category: categories[1], waterPrice: 4400, carbonPrice: 38000, image: '/images/dress1.jpeg'},
+        {name: 'Flare Denim', category: categories[1], waterPrice: 3800, carbonPrice: 33400, image: '/images/jeans5.jpeg'},
+        {name: 'Tan Joggers', category: categories[1], waterPrice: 3500, carbonPrice: 32000, image: '/images/joggers1.jpeg'},
+        {name: 'Nylon Lace Shirt', category: categories[1], waterPrice: 4200, carbonPrice: 40000, image: '/images/blouse2.jpeg'},
+        {name: '2-pack Tank Top', category: categories[0], waterPrice: 5400, carbonPrice: 45600, image: '/images/shirt3.jpeg'},
     ])
     console.log(items)
     process.exit();
