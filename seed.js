78c8285cfb3b560764e6d924cf04e134d884fc44
achasek <chaseknieper@gmail.com>
2022-11-30 22:09:24 -0700

    images render

--- seed.js
+++ seed.js
@@ -15,10 +15,10 @@ require('./config/database');
 
     await Item.deleteMany({});
     const items = await Item.create([
-        {name: 'Cotton T-Shirt', category: categories[0], waterPrice: 2700, carbonPrice: 22800, image: './public/images/shirt1.jpeg'},
-        {name: 'Slim Fit Denim', category: categories[1], waterPrice: 3800, carbonPrice: 33400, image: './public/images/jeans1.jpeg'},
-        {name: 'Regular Fit Denim', category: categories[0], waterPrice: 3900, carbonPrice: 33400, image: './public/images/jeans1.jpeg'},
-        {name: 'Blouse', category: categories[1], waterPrice: 3000, carbonPrice: 23000, image: './public/images/jeans1.jpeg'},
+        {name: 'Cotton T-Shirt', category: categories[0], waterPrice: 2700, carbonPrice: 22800, image: '/images/shirt1.jpeg'},
+        {name: 'Slim Fit Denim', category: categories[1], waterPrice: 3800, carbonPrice: 33400, image: '/images/jeans1.jpeg'},
+        {name: 'Regular Fit Denim', category: categories[0], waterPrice: 3900, carbonPrice: 33400, image: '/images/jeans1.jpeg'},
+        {name: 'Blouse', category: categories[1], waterPrice: 3000, carbonPrice: 23000, image: '/images/jeans1.jpeg'},
     ])
     console.log(items)
     process.exit();
